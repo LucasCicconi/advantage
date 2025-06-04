3b5207d31eec914cf4e489de573104e5fd106b22
Lucas Cicconi <lucascicconifer@hotmail.com>
2025-06-04 09:44:32 -0300

    Tailwind -> Materials

--- postcss.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {}, // Adiciona prefixos de CSS para compatibilidade com navegadores mais antigos
-  }
-}
--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,11 +1,40 @@
+// src/app/auth/login/login.component.ts
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms'; // Para formulários reativos
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterLink,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrls: ['./login.scss']
 })
 export class Login {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
 
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Formulário de Login Enviado', this.loginForm.value);
+      // Aqui virá a lógica de autenticação com o backend (Cognito)
+    } else {
+      console.log('Formulário inválido');
+    }
+  }
 }
